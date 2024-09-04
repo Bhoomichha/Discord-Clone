c1c01c3eae5748062594ae2d25bf13a5bf310865
YuwenHung <yhung13@my.centennialcollege.ca>
2024-09-04 02:29:44 -0400

    Set up the Dark&Light theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div >
+    <div className="flex flex-col">
       <UserButton
-        afterSignOutUrl="/"/>
+        afterSignOutUrl="/"
+      />
+      <ModeToggle />
     </div>
   );
 }
